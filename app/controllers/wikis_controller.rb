f6d21cce3e6a189f7455a1997f43bb57fe93f225
leo424y <leo424y@gmail.com>
2018-04-27 23:22:54 +0800

    x fraime

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,5 +1,6 @@
 class WikisController < ApplicationController
   def show
+    response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM http://uwikipedia.org"
     q = params[:q]
     video = Wiki.find_by(title: q)
     if video

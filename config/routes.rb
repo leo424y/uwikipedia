d97b8842cdce2bd8a32be0d2dee160685697fd0f
leo424y <leo424y@gmail.com>
2018-05-15 22:01:42 +0800

    fix issue in zh

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: "wikis#show"
   get 'wiki/:q', to: "wikis#show"
+  get 'zh-tw/:q', to: "wikis#show"
+  get 'zh-cn/:q', to: "wikis#show"
+  get 'zh-hk/:q', to: "wikis#show"
+  get 'zh-mo/:q', to: "wikis#show"
+  get 'zh-sg/:q', to: "wikis#show"
   resources :wikis, only: [:update, :new, :show]
 end

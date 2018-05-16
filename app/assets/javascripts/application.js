511bf5c0f0bee0e2e50ab689d054f8c8cb31f967
leo424y <leo424y@gmail.com>
2018-05-16 14:50:00 +0800

    ga

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119313558-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-119313558-1');
-</script>

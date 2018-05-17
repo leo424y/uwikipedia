521a46a6a7e3e73ef5d642fcfc04ea0efbb66a4e
leo424y <leo424y@gmail.com>
2018-05-17 20:57:23 +0800

    disable video relay

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -20,14 +20,14 @@ class WikisController < ApplicationController
       @fullurl = 'https://cofacts.g0v.tw/article/3lrautersil4z'
       @editurl = 'https://cofacts.g0v.tw/reply/JhSq2GIBD3gUWf_HaVvv'
       @title = '大家要自求多福了！最好不吃含有氯化鉀的食品！台鹽 居然成殺手鹽？！請詳細看完…一起關心自己和家人的健康。'
-      @u = 'BM1z1_PTl9Q'
+      # @u = 'BM1z1_PTl9Q'
       @audio = 'en/cofacts'
     elsif (q == 'join')
       @summary = '脊骨神經醫學已是世界衛生組織認可的醫療專業,台灣沒有理由不支持這項專業.脊骨神經醫學已是世界衛生組織認可的專業,台灣沒沒有理由阻止這項在國外學習的好方式幫助台灣人民.歐美國家醫學比台灣先進都有脊骨神經科醫師,台灣政府應該開放脊骨神經科進來,跟上歐美國家.台灣醫學進步,為何無脊骨神經專科及相關法規?現代人長時間使用手機等,衍生出的病變更需要有脊骨神經專科的照護!脊骨神經醫學帶給我希望的曙光.台灣需要與WHO同步,台灣需要這一個專業醫學!'
       @fullurl = 'https://join.gov.tw/idea/detail/e9758e8a-98a2-4903-ae4d-3be9a4970890'
       @editurl = 'https://join.gov.tw/idea/detail/e9758e8a-98a2-4903-ae4d-3be9a4970890'
       @title = '參照<世界衛生組織脊骨神經醫學基礎培訓和安全性指南>成立脊骨神經醫學系,建立脊骨神經醫學制度法規'
-      @u = 'BJQfT9C5Adc'
+      # @u = 'BJQfT9C5Adc'
       @audio = 'en/join'
     else
       @summary = wiki_data.summary
@@ -36,13 +36,13 @@ class WikisController < ApplicationController
       @title = wiki_data.title
       @langlinks = wiki_data.langlinks
       unless params[:q]
-        @u = 'WKM5jRAUgvU'
+        # @u = 'WKM5jRAUgvU'
         @audio = 'wiki/en/Taiwan'
       else
         wiki = Wiki.find_by(title: @title, lang: @sub_domain)
         @audio = "#{@sub_domain}/#{@title}"
         if wiki.present?
-          @u = wiki.videos.last.yid
+          # @u = wiki.videos.last.yid
           wiki.update(count: wiki.count+1)
           # @uwiki = wiki.u
           # (@audio = wiki.title) unless @uwiki
@@ -56,7 +56,7 @@ class WikisController < ApplicationController
           # @uwiki = %x(youtube-dl "ytsearch:#{q} on uWikipedia.org" --get-id)
           # TODO wiki = Wiki.create!(title: q, u: @uwiki)
           wiki = Wiki.create!(title: @title, lang: @sub_domain)
-          wiki.videos.create!(yid: @u)
+          # wiki.videos.create!(yid: @u)
         end
       end
     end
